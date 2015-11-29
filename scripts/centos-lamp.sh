3e3fe2beb1a4e25a5c2d6e52db8ef93ab8181685
Faddah Wolf <my_biz@me.com>
2015-11-28 23:09:34 -0800

    adding all files for a CentOS 6.5 LAMP Stack

---
+++ scripts/centos-lamp.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Get what yum will need for Node.js install.
+curl --silent --location https://rpm.nodesource.com/setup | bash -
+# Update CentOS with any patches
+yum update -y --exclude-kernel
+# Tools
+yum install -y nano vim git unzip screen nodejs
+# Apache
+yum install -y httpd httpd-devel httpd-tools
+chkconfig --add httpd
+chkconfig httpd on
+service httpd stop
+
+rm -rf /var/www/html
+ln -s /vagrant /var/www/html
+
+service httpd start
+
+# PHP
+yum install -y php php-cli php-common php-devel php-mysql
+
+# MySQL
+yum install -y mysql mysql-server mysql-devel
+chkconfig --add mysqld
+chkconfig mysqld on
+service mysqld stop
+
+service mysqld start
+
+mysql -u root -e "SHOW DATABASES";
+
+# Download Starter Content
+
+service httpd restart
+
+yum groupinstall -y 'Development Tools'
+sudo npm cache clean -f
+sudo npm install -g n
+sudo n 4.2.2
+sudo npm install -g npm@3.3.12
+sudo npm i -g lodash@3.0.0 bower browserify connect express grunt grunt-cli gulp gulp-cli horse jasmine mocha require socket.io typescript webpack
