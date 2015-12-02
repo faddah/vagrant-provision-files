803d4f8ee027a23a83e63fa19ef2a308e3314d4b
Faddah Wolf <my_biz@me.com>
2015-12-01 22:22:56 -0800

    added sysvinit-utils for "service" to ubuntu-common.sh and made changes ubuntu-web.sh to get apache2 & php5 installs running properly.

--- scripts/ubuntu-trusty/ubuntu-web.sh
+++ scripts/ubuntu-trusty/ubuntu-web.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
 # Apache
-sudo apt-get install apache2-bin apache2-dev
-chkconfig --add httpd
-chkconfig httpd on
-service httpd stop
+sudo apt-get install --yes apache2-bin apache2-dev
+# chkconfig --add httpd
+# chkconfig httpd on
+# service httpd stop
 
 rm -rf /var/www/html
 ln -s /vagrant /var/www/html
 
-service httpd start
+# service httpd start
 
 # PHP
-sudo apt-get install php5 php5-cli php5-common php5-cgi php5-dev php5-mysql phpmyadmin
+sudo apt-get install --yes php5 php5-cli php5-common php5-cgi php5-dev php5-mysql phpmyadmin
 
 # Download Starter Content
 cd /vagrant
 sudo -u vagrant wget -q https://raw.githubusercontent.com/faddah/vagrant-provision-files/master/files/index.html
 sudo -u vagrant wget -q https://raw.githubusercontent.com/faddah/vagrant-provision-files/master/files/info.php
 
-service httpd restart
+# service httpd restart
 
 cd /
