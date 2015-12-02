d766c78b9fb97764d3ba2f8a11e670dd185ef8ec
Faddah Wolf <my_biz@me.com>
2015-12-01 23:22:24 -0800

    added build-essentials to install in ubuntu-common.sh

--- scripts/ubuntu-trusty/ubuntu-common.sh
+++ scripts/ubuntu-trusty/ubuntu-common.sh
@@ -3,4 +3,4 @@
 # Update Ubuntu apt-get with any patches
 sudo apt-get update
 # Tools
-sudo apt-get install --yes nano vim git git-man sysvinit-utils unzip screen telnet
+sudo apt-get install --yes nano vim git git-man sysvinit-utils unzip screen telnet build-essentials
