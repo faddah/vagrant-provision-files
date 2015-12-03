fdf0a39ebd14cf8a1833f278af3ed6b096bc684c
Faddah Wolf <my_biz@me.com>
2015-12-02 19:43:40 -0800

    changes commenting out lines in ubuntu-web.sh to test and get apache2 web server properly installed and running.

--- scripts/ubuntu-trusty/ubuntu-web.sh
+++ scripts/ubuntu-trusty/ubuntu-web.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
 # Apache
-sudo apt-get install --yes apache2-bin apache2-dev
-# chkconfig --add httpd
-# chkconfig httpd on
-sudo update-rc.d apache2 defaults
-service apache2 stop
+sudo apt-get install --y apache2 apache2-bin apache2-dev
+# sudo update-rc.d apache2 defaults
+# service apache2 stop
 
-cp /var/www/html/index.html /vagrant/index-apache.html
-rm -rf /var/www/html
-ln -s /vagrant /var/www/html
+sudo mv /var/www/html/index.html /vagrant/index-apache.html
+sudo rm -rf /var/www/html
+sudo ln -s /vagrant /var/www/html
 
-service apache2 start
+# service apache2 start
 
 # PHP
 sudo apt-get install --yes php5 php5-cli php5-common php5-cgi php5-dev php5-mysql
@@ -21,6 +19,6 @@ cd /vagrant
 sudo -u vagrant wget -q https://raw.githubusercontent.com/faddah/vagrant-provision-files/master/files/index.html
 sudo -u vagrant wget -q https://raw.githubusercontent.com/faddah/vagrant-provision-files/master/files/info.php
 
-service apache2 restart
+# service apache2 restart
 
 cd /
