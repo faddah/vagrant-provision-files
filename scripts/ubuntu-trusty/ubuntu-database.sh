c726206e9203962b22c6ddb99f45137451f2b0d8
Faddah Wolf <my_biz@me.com>
2015-12-02 23:00:12 -0800

    changed ubuntu-database.sh to install the later stable mysql 5.6

--- scripts/ubuntu-trusty/ubuntu-database.sh
+++ scripts/ubuntu-trusty/ubuntu-database.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # MySQL
-sudo apt-get install --yes mysql-common mysql-server mysql-client mysql-workbench
+sudo apt-get install --yes mysql-server-5.6 mysql-common-5.6 mysql-client-5.6 mysql-workbench
 # chkconfig --add mysqld
 # chkconfig mysqld on
 service mysqld stop
 
 service mysqld start
 
-mysql -u root -p -e "SHOW DATABASES";
+mysql -u root -e "SHOW DATABASES";
 # read -r -p "Enter password: " response
 # response=${response,,}    # tolower
 # if [[ $response =~ ^(^M)$ ]]
