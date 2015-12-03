fa8133dd4fb978da393a2804ba26fb63c998ad2b
Faddah Wolf <my_biz@me.com>
2015-12-02 21:27:51 -0800

    added lines from http://www.dev-metal.com/install-setup-php-5-6-ubuntu-14-04-lts to ubuntu-web.sh to make sure php is updated to 5.6.x

--- scripts/ubuntu-trusty/ubuntu-web.sh
+++ scripts/ubuntu-trusty/ubuntu-web.sh
@@ -12,6 +12,17 @@ sudo ln -s /vagrant /var/www/html
 sudo service apache2 start
 
 # PHP
+# from http://www.dev-metal.com/install-setup-php-5-6-ubuntu-14-04-lts/ - add apt repository ppa that has latest php 5.6.x
+sudo add-apt-repository ppa:ondrej/php5-5.6
+
+# update apt-get and apt-get latest python software properties to make all this php install work
+sudo apt-get update
+sudo apt-get install python-software-properties
+
+# run apt-get update again after all that
+sudo apt-get update
+
+# install all the php5.6.x
 sudo apt-get install --yes php5 php5-cli php5-common php5-cgi php5-dev php5-mysql
 
 # Download Starter Content
