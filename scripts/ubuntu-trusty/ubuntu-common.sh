7115aa47dfb03b72051808a32ffede1273ef3792
Faddah Wolf <my_biz@me.com>
2015-12-05 15:06:14 -0800

    re-did order of statements in ubuntu-nodejs.sh to make sure permission fixes get run properly, added echo statements to let us know where we're at in both ubuntu-common.sh and ubuntu-nodejs.sh.

--- scripts/ubuntu-trusty/ubuntu-common.sh
+++ scripts/ubuntu-trusty/ubuntu-common.sh
@@ -2,9 +2,8 @@
 
 # For use with the Atlas Vagrant box, ubuntu/trusty64
 # Update Ubuntu apt-get with any patches
-
-echo -e "\n--- Updating packages list ---\n"
+echo -e "\n\n--- Updating packages list ---\n\n"
 sudo apt-get update
 # Tools
-echo -e "\n--- Installing basic common Dev tools... ---\n"
+echo -e "\n\n--- Installing basic common Dev tools... ---\n\n"
 sudo apt-get install --yes build-essential git git-man unzip python-software-properties
--- scripts/ubuntu-trusty/ubuntu-nodejs.sh
+++ scripts/ubuntu-trusty/ubuntu-nodejs.sh
@@ -1,26 +1,31 @@
 #!/bin/bash
 
-# Update Apt
-sudo apt-get update
-
 # Get what apt-get will need for Node.js install.
+echo -e "\n\n--- Get source needed for Node.js install... ---\n\n"
 curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
 
-# Install node.js & npm with apt-get
+# Update Apt
+echo -e "\n\n--- Updating apt-get... ---\n\n"
+sudo apt-get update
 
+# Install node.js & npm with apt-get
+echo -e "\n\n--- Starting Node.js install... ---\n\n"
 sudo apt-get install --yes nodejs
 
-# Fixes for the node.js & npm directories
-mkdir ~/npm-global
+# Remove any unnecessary packages from Apt
+echo -e "\n\n--- Node.js install - done-zo! Removing anything unnecessary from apt-get... ---\n\n"
+sudo apt-get autoremove
+
+# Fixes for permissions of the node.js & npm directories
+echo -e "\n\n--- Starting Fixes for Node.js & NPM permissions... ---\n\n"
+sudo mkdir ~/npm-global
 sudo npm config set prefix '~/npm-global'
 sudo rm -rf ~/.profile
 sudo mv ~/profile-nodejs ~/.profile
 source ~/.profile
 
-# Remove any unnecessary packages from Apt
-
-sudo apt-get autoremove
-
+# Desired NPM Packages install
+echo -e "\n\n--- Starting NPM Extra Packages install... ---\n\n"
 sudo npm cache clean -f
 # sudo npm install -g n
 # sudo n 4.2.2
@@ -29,4 +34,5 @@ sudo npm i -g lodash@3.10.1 bower browserify connect express grunt grunt-cli gul
 
 sudo npm list -g --depth=0
 
+echo -e "\n\n--- Node.js & NPM install... all done! ---\n\n"
 # ...done!
