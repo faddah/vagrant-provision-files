3e803ca241bfc3aa9dbddb11a74608b4b56282f3
Faddah Wolf <my_biz@me.com>
2015-12-05 11:04:52 -0800

    added a couple of echo statements to ubuntu-common.sh so we know where we're at in the install.

--- scripts/ubuntu-trusty/ubuntu-common.sh
+++ scripts/ubuntu-trusty/ubuntu-common.sh
@@ -2,6 +2,9 @@
 
 # For use with the Atlas Vagrant box, ubuntu/trusty64
 # Update Ubuntu apt-get with any patches
+
+echo -e "\n--- Updating packages list ---\n"
 sudo apt-get update
 # Tools
+echo -e "\n--- Installing basic common Dev tools... ---\n"
 sudo apt-get install --yes build-essential git git-man unzip python-software-properties
