5f08afff8f655c5c64606bc06466f0a0e1e7ef46
Faddah Wolf <my_biz@me.com>
2015-12-04 19:06:39 -0800

    added sudo to the front of some lines to make it work in this ubuntu install.

--- scripts/ubuntu-trusty/ubuntu-database.sh
+++ scripts/ubuntu-trusty/ubuntu-database.sh
@@ -3,11 +3,11 @@
 # MySQL
 
 # Set-up so when MySQL install asks for password, it gets it.
-debconf-set-selections <<< 'mysql-server mysql-server/root_password password mysqld'
-debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password mysqld'
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password mysqld'
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password mysqld'
 
 # Auto-update again so it gets those changes.
-apt-get update
+sudo apt-get update
 
 sudo apt-get install --yes mysql-server-5.6 mysql-common-5.6 mysql-client-5.6
 # chkconfig --add mysqld
